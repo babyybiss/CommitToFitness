6a256fab2ad47103b223a9a51f1a4d7a48d99f55
babyybiss <hyunni.dev@gmail.com>
2024-07-25 17:19:51 +0900

    ecs deploy config

--- main.py
+++ main.py
@@ -44,7 +44,12 @@ squat_counter = SquatCounter()
         
 @app.get("/")
 def root(req: Request):
+    return "alive"
+
+@app.get("/health_check")
+def root(req: Request):
     return templates.TemplateResponse("index.html", {"request": req})
+
 
 @app.get("/squats/counting")
 def getCountingTemplate(req: Request):
